e77375f63631beedfce47fc2cd00ede719d43772
EbenZhang <zhangyingneng@gmail.com>
2014-11-19 23:03:03 +0800

    Fixed #4 Create image in memory

--- PdfSharp.Xps/PdfSharp.Xps.Rendering/ImageBuilder.cs
+++ PdfSharp.Xps/PdfSharp.Xps.Rendering/ImageBuilder.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Media;
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Data;
+using System.Windows.Media;
 using PdfSharp.Drawing;
 using System;
 using System.Windows.Media.Imaging;
@@ -22,28 +26,67 @@ namespace PdfSharp.Xps.Rendering
 
     private XImage Build(ImageBrush brush)
     {
-      //FixedPage fpage = brush.GetParent<FixedPage>();
-      //if (fpage == null)
-      // Debug.Assert(false);
-
-      //FixedPayload payload = fpage.Document.Payload;  // TODO: find better way to get FixedPayload
-      //Debug.Assert(Object.Equals(payload, Context.
-
-      // Get the font object.
-      // Important: font.PdfFont is not yet defined here on the first call
-      //string uriString = (brush.ImageSource as BitmapImage);
-      //BitmapSource bitmapSource = payload.GetImage(uriString);
-      //eztodo: cache the image....
-      var filePath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), Guid.NewGuid().ToString() + ".png");
-      using (var outStream = System.IO.File.OpenWrite(filePath))
-      {
-        BitmapEncoder enc = new PngBitmapEncoder();
-        enc.Frames.Add(BitmapFrame.Create(brush.ImageSource as BitmapSource));
-        enc.Save(outStream);
-      }
-
-      XImage xpImage = XImage.FromFile(filePath);
+      XpsImage xpImage = new XpsImage(brush.ImageSource as BitmapSource);
       return xpImage;
+    }
+  }
+
+  class XpsImage : XImage
+  {
+    public XpsImage(BitmapSource src)
+    {
+      base.wpfImage = src;
+      var t = typeof (XImage);
+      var fmtProperty = t.GetField("format", BindingFlags.Instance | BindingFlags.NonPublic);
+      fmtProperty.SetValue(this, XImageFormat.Png);
+    }
+
+    public override bool IsCmyk 
+    {
+      get
+      {
+        return false;
+      }
+    }
+
+    public override bool IsJpeg
+    {
+      get
+      {
+        return false;
+      }
+    }
+
+    private MemoryStream memory;
+
+    public override MemoryStream Memory
+    {
+      get
+      {
+        if (memory != null)
+        {
+          return memory;
+        }
+        memory = new MemoryStream();
+        BitmapEncoder enc = new PngBitmapEncoder();
+        if (wpfImage is BitmapFrame)
+        {
+          var frame = wpfImage as BitmapFrame;
+          enc.Frames.Add(frame);
+        }
+        else
+        {
+          enc.Frames.Add(BitmapFrame.Create(wpfImage));
+        }
+        enc.Save(memory);
+        return memory;
+      }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      base.Dispose(disposing);
+      memory.Close();
     }
   }
 }
