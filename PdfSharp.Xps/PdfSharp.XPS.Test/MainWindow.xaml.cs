4db7575ae265811b17f4a3dd4e8f830a6d3bd9fd
EbenZhang <zhangyingneng@gmail.com>
2014-11-20 20:52:08 +0800

    add test project

---
+++ PdfSharp.Xps/PdfSharp.XPS.Test/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace PdfSharp.Xps.Test
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+      var target = @"d:\temp\1.pdf";
+      PdfSharp.Xps.XpsConverter.Convert(@"d:\temp\3.xps", target, 0, false);
+      Process.Start(target);
+    }
+  }
+}
